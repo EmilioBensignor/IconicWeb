80b466c6a2de9d032f23d4ecab7b942cdc3f3b79
Lio <liobensignor@gmail.com>
2024-09-09 15:04:57 -0300

    Modules Images y Icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@primevue/nuxt-module'],
+  modules: ['@primevue/nuxt-module', '@nuxt/image', '@nuxt/icon'],
   app: {
     head: {
       link: [{
