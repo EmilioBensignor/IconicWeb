8743252dc9ae90cfa958dae77aef78bb26231156
Emilio Bensignor <liobensignor@gmail.com>
2024-09-10 13:51:29 -0300

    Configuracion + Default Layout + Main css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  alias: {
+    "@": "/<srcDir>"
+  },
+  css: ["~/assets/main.css"],
   devtools: { enabled: true },
   modules: ['@primevue/nuxt-module', '@nuxt/image', '@nuxt/icon'],
+  colorMode: {
+    preference: "dark",
+    fallback: "dark",
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storageKey: 'nuxt-color-mode'
+  },
   app: {
     head: {
       link: [{
