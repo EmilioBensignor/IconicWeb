aacf6da9233b67e5f297a3c84504a53326e6fe4b
Emilio Bensignor <liobensignor@gmail.com>
2024-09-11 12:54:27 -0300

    Maquetacion mitad Happy Clients

---
+++ components/home/HappyClients.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="w-full columnAlignCenter gap-4 px-3 py-4">
+    <div class="w-full rowSpaceBetweenCenter">
+      <div>
+        <p>+1000</p>
+        <p>Happy clients</p>
+      </div>
+      <div class="gradientLine"></div>
+      <div>
+        <p>+1600</p>
+        <p>Virtual assistants</p>
+      </div>
+    </div>
+    <div class="w-full rowSpaceBetweenCenter">
+      <div>
+        <p>4.9</p>
+        <p>On Google Reviews</p>
+      </div>
+      <div class="gradientLine"></div>
+      <div>
+        <p>24/7</p>
+        <p>Customer support</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+.gradientLine {
+  width: 50px;
+  height: 4px;
+  background: linear-gradient(180deg, var(--color-light-blue), var(--color-blue));
+  transform: rotate(90deg);
+
+}
+</style>
