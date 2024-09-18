4066f06dce7d0c6844a793f7b737c416ff00cea1
Emilio Bensignor <liobensignor@gmail.com>
2024-09-18 18:18:18 -0300

    Header y Footer Menus

--- components/default/Footer.vue
+++ components/default/Footer.vue
@@ -1,29 +1,146 @@
 <template>
   <footer class="column gap-4 bg-dark-blue">
     <div class="column gap-35">
-      <NuxtImg class="logo" src="/images/Iconic-Assistants-Logo.png" alt="Iconic Assistants Logo" />
+      <NuxtImg
+        class="logo"
+        src="/images/Iconic-Assistants-Logo.png"
+        alt="Iconic Assistants Logo" />
       <p>Empowering businesses to thrive.</p>
       <NuxtLink class="w-7 primaryButton" to="/">Contact Us</NuxtLink>
-      <div>
-        // REDES
+      <div class="socialMedia rowCenter gap-35">
+        <a href="facebook.com">
+          <NuxtImg src="/images/footer/Facebook-Icon.svg" alt="Facebook Icon" />
+        </a>
+        <a href="instagram.com">
+          <NuxtImg
+            src="/images/footer/Instagram-Icon.svg"
+            alt="Instagram Icon" />
+        </a>
+        <a href="linkedin.com">
+          <NuxtImg src="/images/footer/LinkedIn-Icon.svg" alt="LinkedIn Icon" />
+        </a>
       </div>
     </div>
-    <div>
-      // Accordions de Links Footer
+    <div class="menuFooter column gap-2">
+      <Accordion value="0">
+        <AccordionPanel
+          v-for="(item, index) in menuAccordion"
+          :key="index"
+          :value="item.value">
+          <AccordionHeader>
+            {{ item.title }}
+          </AccordionHeader>
+          <AccordionContent>
+            <ul class="linksAccordion column gap-3">
+              <li v-for="(link, index) in item.list" :key="index">
+                <NuxtLink :to="link.link">
+                  {{ link.title }}
+                </NuxtLink>
+              </li>
+            </ul>
+          </AccordionContent>
+        </AccordionPanel>
+      </Accordion>
+      <div class="menuBottom column gap-2">
+        <NuxtLink
+          v-for="(link, index) in menuBottom"
+          :key="index"
+          class="text-white no-underline"
+          :to="link.route">
+          {{ link.title }}
+        </NuxtLink>
+      </div>
     </div>
-    <div class="mt-2">
-      // Privacy | Terms
-      // Right Reserverd 2024?
+    <div class="w-full columnAlignCenter gap-35 mt-2">
+      <div class="privacyTerms rowCenter gap-3">
+        <NuxtLink to="#" class="text-white no-underline">Privacy</NuxtLink>
+        <span>|</span>
+        <NuxtLink to="#" class="text-white no-underline">Terms</NuxtLink>
+      </div>
+      <div class="rightsReserved columnAlignCenter">
+        <p>© Iconic Assistants. All Rights Reserved {{ currentYear }}</p>
+      </div>
     </div>
   </footer>
 </template>
 
-<style scoped>
-footer {
-  padding: 1.875rem 1rem;
-}
+<script>
+  export default {
+    data() {
+      return {
+        menuAccordion: null,
+        menuBottom: null,
+        currentYear: new Date().getFullYear(),
+      };
+    },
+    async created() {
+      try {
+        this.menuAccordion = await fetch("/menu/menuAccordion.json").then((res) => res.json());
+        this.menuBottom = await fetch("/menu/menuLinks.json").then((res) => res.json());
+      } catch (error) {
+        console.error("Error fetching JSON data:", error);
+      }
+    },
+  };
+</script>
 
-.logo {
-  width: 6.25rem;
-}
+<style>
+  .menuFooter .p-accordionheader {
+    color: var(--color-white) !important;
+    font-weight: 700;
+    border-bottom: 1px solid var(--color-grey-50) !important;
+    padding: 0.5rem 0;
+  }
+
+  .menuFooter
+    .p-accordionpanel:not(.p-disabled).p-accordionpanel-active
+    > .p-accordionheader {
+    background: transparent !important;
+    border: none !important;
+    color: var(--color-white) !important;
+  }
+
+  .menuFooter
+    .p-accordionpanel:not(.p-disabled).p-accordionpanel-active
+    > .p-accordionheader
+    .p-accordionheader-toggle-icon {
+    color: var(--color-white) !important;
+  }
+
+  .menuFooter .p-accordioncontent-content {
+    border-bottom: 1px solid var(--color-grey-50) !important;
+    padding: 0.5rem 0;
+  }
+</style>
+
+<style scoped>
+  footer {
+    padding: 1.875rem 1rem;
+  }
+
+  .logo {
+    width: 6.25rem;
+  }
+
+  .socialMedia a img {
+    width: 2rem;
+    height: 2rem;
+  }
+
+  .linksAccordion li a {
+    font-size: 0.75rem;
+    cursor: pointer;
+  }
+
+  .menuBottom a {
+    font-weight: 700;
+  }
+
+  .privacyTerms a {
+    font-size: 0.75rem;
+  }
+
+  .rightsReserved p {
+    font-size: 00.75rem;
+  }
 </style>
--- components/default/Header.vue
+++ components/default/Header.vue
@@ -1,15 +1,126 @@
 <template>
-  <header class="columnAlignCenter">
-    <NuxtImg class="logo" src="/images/Iconic-Assistants-Logo.png" alt="Iconic Assistants Logo" />
+  <header class="headerDefault allCenter sticky top-0">
+    <button class="hamburger absolute" @click="toggleDrawer">
+      <Icon class="text-white" size="2rem" name="mingcute:menu-fill" />
+    </button>
+    <NuxtImg
+      class="logo"
+      src="/images/Iconic-Assistants-Logo.png"
+      alt="Iconic Assistants Logo" />
+    <div>
+      <Drawer
+        :visible="drawerMenu"
+        :show-close-button="false"
+        @click-outside="closeDrawer">
+        <div class="menuContent column">
+          <NuxtLink to="#" class="primaryButton">Get Started</NuxtLink>
+          <div v-for="(accordion, index) in menuAccordion" :key="index">
+            <p class="font-bold">{{ accordion.title }}</p>
+            <ul>
+              <li v-for="(link, index) in accordion.list" :key="index">
+                <NuxtLink :to="link.route" class="text-white no-underline">{{
+                  link.title
+                }}</NuxtLink>
+              </li>
+            </ul>
+          </div>
+          <div class="column">
+            <NuxtLink
+              :to="link.route"
+              v-for="(link, index) in menuLinks"
+              class="text-white font-bold no-underline"
+              :key="index"
+              >{{ link.title }}</NuxtLink
+            >
+          </div>
+        </div>
+      </Drawer>
+    </div>
   </header>
 </template>
 
-<style scoped>
-header {
-  padding: 1.125rem 1rem;
-}
+<script>
+  export default {
+    data() {
+      return {
+        drawerMenu: false,
+        menuAccordion: null,
+        menuLinks: null,
+      };
+    },
+    methods: {
+      toggleDrawer() {
+        this.drawerMenu = !this.drawerMenu;
+      },
+      closeDrawer() {
+        this.drawerMenu = false;
+      },
+      handleClickOutside(event) {
+        const drawer = this.$refs.drawer;
+        if (drawer && !drawer.contains(event.target)) {
+          this.closeDrawer();
+        }
+      },
+    },
+    async created() {
+      try {
+        this.menuAccordion = await fetch("/menu/menuAccordion.json").then(
+          (res) => res.json()
+        );
+        this.menuLinks = await fetch("/menu/menuLinks.json").then((res) =>
+          res.json()
+        );
+      } catch (error) {
+        console.error("Error fetching JSON data:", error);
+      }
+    },
+    mounted() {
+      document.addEventListener("click", this.handleClickOutside);
+    },
+    beforeDestroy() {
+      document.removeEventListener("click", this.handleClickOutside);
+    },
+  };
+</script>
 
-.logo {
-  width: 6.25rem;
-}
+<style>
+  .p-drawer-mask {
+    width: 60% !important;
+    top: 4.438rem !important;
+  }
+
+  .p-drawer-header {
+    display: none !important;
+  }
+
+  .p-drawer-content {
+    background: linear-gradient(
+      180deg,
+      #0d1428 0%,
+      #0e1a3d 50%,
+      #0f2052 100%
+    ) !important;
+    padding: 1rem !important;
+  }
+</style>
+
+<style scoped>
+  header {
+    padding: 1.125rem 1rem;
+  }
+
+  .hamburger {
+    left: 1rem;
+    background: none;
+    border: none;
+    cursor: pointer;
+  }
+
+  .logo {
+    width: 6.25rem;
+  }
+
+  .menuContent {
+    gap: 1.875rem;
+  }
 </style>
