99e337c8f75cd8d20164f45783b792eae0116e53
Lio <liobensignor@gmail.com>
2024-09-23 19:31:37 -0300

    Cambio de Ruta Reviews

--- components/home/googleReviews/ReviewCard.vue
+++ components/home/googleReviews/ReviewCard.vue
@@ -4,7 +4,7 @@
       <div
         class="avatar bgCover"
         :style="{
-          backgroundImage: `url('/images/home/reviews/${review.img}-Google-Review.png')`,
+          backgroundImage: `url('/images/reviews/${review.img}-Google-Review.png')`,
         }"
       ></div>
       <div class="reviewHeader column gap-1">
