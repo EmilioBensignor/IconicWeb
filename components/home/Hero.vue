2114bef70876c5004be778799272ca2af637bf72
Emilio Bensignor <liobensignor@gmail.com>
2024-10-10 12:30:28 -0300

    Mejoras de Seo + Robots

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -21,261 +21,285 @@
       </div>
     </div>
     <div class="w-full columnAlignCenter">
-      <div class="w-full heroAssistant bgContain"></div>
+      <NuxtImg
+        class="heroAssistant assistantMobile"
+        src="/images/home/Iconic-Executive-Assistant-Recruitment-Mobile.webp"
+        alt="Iconic-Executive-Assistant-Recruitment" />
+      <NuxtImg
+        class="heroAssistant assistantDeskop"
+        src="/images/home/Iconic-Executive-Assistant-Recruitment-Desktop.webp"
+        alt="Iconic-Executive-Assistant-Recruitment" />
+      <NuxtImg
+        class="heroAssistant assistantLargeDeskop"
+        src="/images/home/Iconic-Executive-Assistant-Recruitment-Large.webp"
+        alt="Iconic-Executive-Assistant-Recruitment" />
     </div>
   </section>
 </template>
 
 <script>
-import ROUTES_NAMES from "~/constants/ROUTES_NAMES.js";
+  import ROUTES_NAMES from "~/constants/ROUTES_NAMES.js";
 
-export default {
-  data() {
-    return {
-      routes: ROUTES_NAMES,
-    };
-  },
-};
+  export default {
+    data() {
+      return {
+        routes: ROUTES_NAMES,
+      };
+    },
+  };
 </script>
 
 <style scoped>
-.heroAssistant {
-  max-width: 350px;
-  height: 16.875rem;
-  background-image: url("/images/home/Iconic-Executive-Assistant-Recruitment.webp");
-}
-
-.empower {
-  font-weight: 700;
-}
-
-h1 {
-  font-weight: 800;
-}
-
-.starting {
-  font-weight: 500;
-  max-width: 320px;
-}
-
-.starting span {
-  font-weight: 800;
-}
-
-.assistantLargeImg {
-  display: none;
-}
-
-@media (width >= 400px) {
   .heroAssistant {
-    height: 21.875rem;
-  }
-}
-
-@media (width >= 480px) {
-  .hero {
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    gap: 1rem !important;
-    padding: 1rem 2rem !important;
-  }
-
-  .hero > div {
-    width: 50% !important;
-  }
-
-  .hero > div:first-of-type {
-    align-items: flex-start;
-    gap: 0.5rem !important;
-    padding: 0 !important;
-  }
-
-  .hero > div:first-of-type > div {
-    align-items: flex-start;
+    max-width: 350px;
+    height: 16.875rem;
+    object-fit: contain;
   }
 
   .empower {
-    text-align: start !important;
-    font-size: 1.25rem;
+    font-weight: 700;
   }
 
   h1 {
-    text-align: start !important;
-    font-size: 1.75rem;
+    font-weight: 800;
   }
 
   .starting {
-    max-width: 100%;
-    text-align: start !important;
+    font-weight: 500;
+    max-width: 320px;
   }
 
-  .hero > div:first-child > div:last-of-type {
-    margin-top: 0.5rem;
+  .starting span {
+    font-weight: 800;
   }
 
-  .heroAssistant {
-    max-width: 100%;
-    height: 17.5rem;
-  }
-}
-
-@media (width >= 700px) {
-  .hero {
-    justify-content: center;
-    gap: 1.25rem !important;
-    padding: 2.5rem !important;
+  .assistantLargeDeskop,
+  .assistantDeskop {
+    display: none;
   }
 
-  .hero > div:first-of-type {
-    width: 46%;
-    gap: 1rem !important;
+  @media (width >= 400px) {
+    .heroAssistant {
+      height: 21.875rem;
+    }
   }
 
-  .empower {
-    font-size: 1.5rem;
+  @media (width >= 480px) {
+    .hero {
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      gap: 1rem !important;
+      padding: 1rem 2rem !important;
+    }
+
+    .hero > div {
+      width: 50% !important;
+    }
+
+    .hero > div:first-of-type {
+      align-items: flex-start;
+      gap: 0.5rem !important;
+      padding: 0 !important;
+    }
+
+    .hero > div:first-of-type > div {
+      align-items: flex-start;
+    }
+
+    .empower {
+      text-align: start !important;
+      font-size: 1.25rem;
+    }
+
+    h1 {
+      text-align: start !important;
+      font-size: 1.75rem;
+    }
+
+    .starting {
+      max-width: 100%;
+      text-align: start !important;
+    }
+
+    .hero > div:first-child > div:last-of-type {
+      margin-top: 0.5rem;
+    }
+
+    .heroAssistant {
+      max-width: 100%;
+      height: 17.5rem;
+    }
   }
 
-  h1 {
-    font-size: 2rem;
+  @media (width >= 700px) {
+    .hero {
+      justify-content: center;
+      gap: 1.25rem !important;
+      padding: 2.5rem !important;
+    }
+
+    .hero > div:first-of-type {
+      width: 46%;
+      gap: 1rem !important;
+    }
+
+    .empower {
+      font-size: 1.5rem;
+    }
+
+    h1 {
+      font-size: 2rem;
+    }
+
+    .starting {
+      font-size: 1rem;
+    }
+
+    .hero > div:first-child > div:last-of-type {
+      margin-top: 0;
+    }
+
+    .hero > div:last-of-type {
+      width: 54%;
+    }
+
+    .heroAssistant {
+      height: 19rem;
+    }
   }
 
-  .starting {
-    font-size: 1rem;
+  @media (width >= 850px) {
+    .empower {
+      font-size: 1.875rem;
+    }
+
+    h1 {
+      font-size: 2.5rem;
+    }
+
+    .starting {
+      font-size: 1.125rem;
+    }
+
+    .heroAssistant {
+      height: 21rem;
+    }
   }
 
-  .hero > div:first-child > div:last-of-type {
-    margin-top: 0;
+  @media (width >= 1080px) {
+    .hero {
+      padding: 2.5rem 5rem !important;
+    }
+
+    .hero > div:first-of-type {
+      width: 50%;
+    }
+
+    .hero > div:last-of-type {
+      width: 50%;
+    }
+
+    .empower {
+      font-size: 2rem;
+    }
+
+    h1 {
+      font-size: 3.25rem;
+    }
+
+    .starting {
+      font-size: 1.25rem;
+    }
+
+    .assistantMobile {
+      display: none;
+    }
+
+    .assistantDeskop {
+      display: block;
+    }
+
+    .heroAssistant {
+      height: 25rem;
+    }
   }
 
-  .hero > div:last-of-type {
-    width: 54%;
+  @media (width >= 1280px) {
+    .starting {
+      max-width: 583px;
+    }
+
+    .heroAssistant {
+      height: 27rem;
+    }
   }
 
-  .heroAssistant {
-    height: 19rem;
-  }
-}
+  @media (width >= 1440px) {
+    .hero {
+      padding: 2.5rem 8rem !important;
+    }
 
-@media (width >= 850px) {
-  .empower {
-    font-size: 1.875rem;
-  }
+    .hero > div:first-of-type {
+      gap: 2rem !important;
+    }
 
-  h1 {
-    font-size: 2.5rem;
-  }
+    .empower {
+      font-size: 2.125rem;
+    }
 
-  .starting {
-    font-size: 1.125rem;
-  }
+    h1 {
+      font-size: 3.5rem;
+      margin-top: 0.75rem;
+    }
 
-  .heroAssistant {
-    height: 21rem;
-  }
-}
+    .starting {
+      font-size: 1.375rem;
+    }
 
-@media (width >= 1080px) {
-  .hero {
-    padding: 2.5rem 5rem !important;
-  }
+    .assistantDeskop {
+      display: none;
+    }
 
-  .hero > div:first-of-type {
-    width: 50%;
-  }
-
-  .hero > div:last-of-type {
-    width: 50%;
+    .assistantLargeDeskop {
+      display: block;
+    }
   }
 
-  .empower {
-    font-size: 2rem;
+  @media (width >= 1600px) {
+    .empower {
+      font-size: 2.25rem;
+    }
+
+    h1 {
+      font-size: 3.625rem;
+    }
+
+    .starting {
+      font-size: 1.5rem;
+    }
+
+    .heroAssistant {
+      max-width: 680px;
+    }
   }
 
-  h1 {
-    font-size: 3.25rem;
-  }
+  @media (width >= 1920px) {
+    .hero > div:first-of-type {
+      gap: 2.5rem !important;
+    }
 
-  .starting {
-    font-size: 1.25rem;
-  }
+    .empower {
+      font-size: 2.375rem;
+    }
 
-  .heroAssistant {
-    height: 25rem;
-  }
-}
+    h1 {
+      font-size: 3.625rem;
+    }
 
-@media (width >= 1280px) {
-  .starting {
-    max-width: 583px;
+    .starting {
+      max-width: 605px;
+      font-size: 1.625rem;
+    }
   }
-
-  .heroAssistant {
-    height: 27rem;
-  }
-}
-
-@media (width >= 1440px) {
-  .hero {
-    padding: 2.5rem 8rem !important;
-  }
-
-  .hero > div:first-of-type {
-    gap: 2rem !important;
-  }
-
-  .empower {
-    font-size: 2.125rem;
-  }
-
-  h1 {
-    font-size: 3.5rem;
-    margin-top: 0.75rem;
-  }
-
-  .starting {
-    font-size: 1.375rem;
-  }
-
-  .heroAssistant {
-    background-image: url("/images/home/Iconic-Executive-Assistant-Recruitment-Large.webp");
-  }
-}
-
-@media (width >= 1600px) {
-  .empower {
-    font-size: 2.25rem;
-  }
-
-  h1 {
-    font-size: 3.625rem;
-  }
-
-  .starting {
-    font-size: 1.5rem;
-  }
-
-  .heroAssistant {
-    max-width: 680px;
-  }
-}
-
-@media (width >= 1920px) {
-  .hero > div:first-of-type {
-    gap: 2.5rem !important;
-  }
-
-  .empower {
-    font-size: 2.375rem;
-  }
-
-  h1 {
-    font-size: 3.625rem;
-  }
-
-  .starting {
-    max-width: 605px;
-    font-size: 1.625rem;
-  }
-}
 </style>
--- pages/get-started.vue
+++ pages/get-started.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <ContactHero />
+    <HomeHero />
     <HomeHappyClients />
     <HomeHowWeHelp />
     <HomeMaximizeEfficiency
